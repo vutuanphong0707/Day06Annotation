f219d399f35aea2a800b7b7fd13f6d7d5c84bc21
Vu Tuan Phong <vutuanphong0707@gmail.com>
2025-10-02 21:34:43 +0700

    First commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Lab06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab06.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Giả lập danh sách account (thay cho database)
+        private static List<Account> accounts = new List<Account>();
+
+        // GET: Account/Index
+        public IActionResult Index()
+        {
+            return View(accounts);
+        }
+
+        // GET: Account/Create
+        public IActionResult Create()
+        {
+            Account model = new Account();
+            return View(model);
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Account model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = accounts.Count + 1;
+                accounts.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // Remote Validation cho Phone
+        [AcceptVerbs("GET", "POST")]
+        public IActionResult VerifyPhone(string phone)
+        {
+            Regex _isPhone = new Regex(@"^(0[0-9]{9})$|^(0[0-9]{2})[.\s]?[0-9]{3}[.\s]?[0-9]{4}$");
+            if (!_isPhone.IsMatch(phone))
+            {
+                return Json($"Số điện thoại {phone} không đúng định dạng, VD: 0986421127 hoặc 098.421.1127");
+            }
+
+            return Json(true);
+        }
+
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab06.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "Họ tên không được để trống")]
+        [StringLength(50, ErrorMessage = "Họ tên không vượt quá 50 ký tự")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Định dạng email không hợp lệ")]
+        public string Email { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        [Remote(action: "VerifyPhone", controller: "Account")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Địa chỉ")]
+        public string Address { get; set; }
+
+        [Display(Name = "Ảnh đại diện")]
+        public string Avatar { get; set; }
+
+        [Display(Name = "Ngày sinh")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "Giới tính")]
+        public string Gender { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Mật khẩu phải từ 6 đến 20 ký tự")]
+        public string Password { get; set; }
+
+        [Display(Name = "Facebook")]
+        [Url(ErrorMessage = "Đường dẫn Facebook không hợp lệ")]
+        public string Facebook { get; set; }
+    }
+}
